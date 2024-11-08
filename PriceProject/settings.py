712fb6aa6c8457bfbf25cb3241edafe4fc0e291c
hdhankecha <hdhankecha456@rku.ac.in>
2024-11-08 16:21:22 +0530

    changes makes

--- PriceProject/settings.py
+++ PriceProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y9vuak6ji1(oo^a1ol5fo_xy^9i*z*zylpten93bzq==&%4lsp'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['.vercel.app','*','priceprojet.onrender.com']
 
